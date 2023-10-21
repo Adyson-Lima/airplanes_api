58dd2f7a257349519e4b10f0179b3601b96032d3
adyson <adyv8@yahoo.com.br>
2023-10-21 09:11:41 -0300

    13° commit - tdd controller: Consegue listar todos os airplanes e retornar status 200? GREEN

--- app/controllers/api/v1/airplanes_controller.rb
+++ app/controllers/api/v1/airplanes_controller.rb
@@ -1,2 +1,20 @@
 class Api::V1::AirplanesController < ApplicationController
+  
+  before_action :set_airplane, only: %i[show update destroy]
+  
+  def index
+    @airplanes = Airplane.all
+    render json: @airplanes
+  end
+  
+private
+
+def set_airplane
+  @airplane = Airplane.find(params[:id])
+end
+
+def airplane_params
+  params.require(:airplane).permit(:name, :description, :manufacturer)
+end
+  
 end
