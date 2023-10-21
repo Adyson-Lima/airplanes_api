467bea828e8fbf6027a257ebda74d9b7f4435d11
adyson <adyv8@yahoo.com.br>
2023-10-21 09:17:17 -0300

    14° commit - tdd controller: Consegue retornar um airplane especifico e retornar status 200? RED

--- spec/controllers/v1/airplanes_controller_v1_spec.rb
+++ spec/controllers/v1/airplanes_controller_v1_spec.rb
@@ -13,4 +13,12 @@ RSpec.describe Api::V1::AirplanesController, type: :controller do
     end
   end
   
+  describe 'GET /api/v1/airplanes/id' do
+    it 'Consegue retornar um airplane especifico e retornar status 200?' do
+      get :show, params: {id: @airplane.id}
+      expect(response.body).to include_json(id: @airplane.id)
+      expect(response).to have_http_status(200)
+    end
+  end
+  
 end
