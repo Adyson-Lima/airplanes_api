590c96c312c37634d053de3a43ca2249c440ebb6
adyson <adyv8@yahoo.com.br>
2023-10-21 08:24:53 -0300

    4° commit - tdd model: description consegue ser preenchido? RED

--- spec/models/airplane_spec.rb
+++ spec/models/airplane_spec.rb
@@ -11,6 +11,11 @@ RSpec.describe Airplane, type: :model do
       expect(@airplane.name).to eq("a320")
     end
     
+    it 'description consegue ser preenchido?' do
+      @airplane.description = ""
+      expect(@airplane.description).to eq("Um avião bem conceituado")
+    end
+    
   end
   
 end
